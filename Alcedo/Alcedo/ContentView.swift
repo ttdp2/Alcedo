20c6478b10bf9d6325e36b8bb0eec0a0cedce537
ttdp <tiantong.dev@gmail.com>
2020-02-10 16:20:14 +0800

    Add Alcedo

---
+++ Alcedo/Alcedo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Alcedo
+//
+//  Created by Tian Tong on 2020/2/10.
+//  Copyright © 2020 TTDP. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
